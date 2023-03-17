13e7463853b0077b06d39772e1bb85d1d0b0f518
DESKTOP-DOGVPUB\SSAFY <jjh099@icloud.com>
2023-03-17 17:55:32 +0900

    ORM, model, url 복습

--- 03_day/review1/articles/models.py
+++ 03_day/review1/articles/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # articles/models.py
-class Article(models.Model):
+class Article(models.Model): #article app에서 사용할 게시글 Article
     title = models.CharField(max_length=20) # models에서 title은 문자열 형태, 글자수 지정
     content = models.TextField() # 글자수 지정없이 사용 가능
 
+ 
